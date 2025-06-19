3d3dbb744feb4a64a74a81531f65d4c8d0b3a2d5
shalini1908-code <shalogu19@gmail.com>
2025-06-19 20:34:50 +0530

    Update SingletonTest.java

--- WEEK1/Design patterns and principles/EXERCISE-1/SingletonTest.java
+++ WEEK1/Design patterns and principles/EXERCISE-1/SingletonTest.java
@@ -1,1 +1,14 @@
+public class SingletonTest {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
 
+        logger1.log("This is a test message.");
+
+        if (logger1 == logger2) {
+            System.out.println("Both logger1 and logger2 refer to the same instance.");
+        } else {
+            System.out.println("logger1 and logger2 are different instances.");
+        }
+    }
+}
